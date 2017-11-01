5bc6da6bbf9581e24cf5201d20e0a4b458d2b6a7
chucooleg <chucooleg@gmail.com>
2017-10-31 20:14:50 -0700

    generic update

---
+++ code/pilot_results_WIP.r
@@ -0,0 +1,7 @@
+
+
+qualtric_data_path = "../qualtric_data/20171028_qualtric_results_pilot_0.25.csv"
+
+current_task_data = get_current_task_data(qualtric_data_path)
+
+convert_raw_to_correct_ans(current_task_data, allQ)
